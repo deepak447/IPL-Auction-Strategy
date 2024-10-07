f75882d4a8c7535819b794280568522c16ba04cf
deepak447 <109805323+deepak447@users.noreply.github.com>
2024-10-07 17:05:34 +0530

    Update Schemas.sql

--- Schemas.sql
+++ Schemas.sql
@@ -1,20 +1,44 @@
--- SCHEMAS of Netflix
+-- SCHEMAS of IPL Data
 
-DROP TABLE IF EXISTS netflix;
-CREATE TABLE netflix
-(
-	show_id	VARCHAR(5),
-	type    VARCHAR(10),
-	title	VARCHAR(250),
-	director VARCHAR(550),
-	casts	VARCHAR(1050),
-	country	VARCHAR(550),
-	date_added	VARCHAR(55),
-	release_year	INT,
-	rating	VARCHAR(15),
-	duration	VARCHAR(15),
-	listed_in	VARCHAR(250),
-	description VARCHAR(550)
+CREATE TABLE ball_data (
+    id int,
+    inning INT,
+    over INT,
+    ball INT,
+    batsman VARCHAR(255),
+    non_striker VARCHAR(255),
+    bowler VARCHAR(255),
+    batsman_runs INT,
+    extra_runs INT,
+    total_runs INT,
+    is_wicket BOOLEAN,
+    dismissal_kind VARCHAR(255),
+    player_dismissed VARCHAR(255),
+    fielder VARCHAR(255),
+    extras_type VARCHAR(255),
+    batting_team VARCHAR(255),
+    bowling_team VARCHAR(255)
+);
+copy ball_data from 'C:\Program Files\PostgreSQL\16\data\Data\IPL Dataset\IPL_Ball.csv' delimiter ','csv header; 
+
+CREATE TABLE match_data (
+	id integer,
+	city varchar(255),
+	date date,
+	player_of_match varchar(255),
+	venue varchar(255),
+	neutral_venue int,
+	team1 varchar(255),
+	team2 varchar(255),
+	toss_winner varchar(255),
+	toss_decision varchar(255),
+	winner varchar(255),
+	result varchar(255),
+	result_margin varchar(255),
+	eliminator varchar(255),
+	method varchar(255),
+	umpire1 varchar(255),
+	umpire2 varchar(255)
 );
 
-SELECT * FROM netflix;
+copy matches from 'C:\Program Files\PostgreSQL\16\data\Data\IPL Dataset\IPL_matches.csv' delimiter ','csv header;
